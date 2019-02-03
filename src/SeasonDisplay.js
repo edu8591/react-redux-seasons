05292197d8eb037f50d8fb125e5e8d5dfb7fc54d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2019-02-03 12:40:48 -0500

    class components,  state, lifecycle methods

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,39 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold!',
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'summer' : 'winter';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    
+    //this way of doing this is too repetitive so it was changed to use the seasonConfig object
+    // const text = season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    // const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
     return (
-        <div>Season Display</div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 
 
@@ -11,29 +13,35 @@ class App extends React.Component {
     //this object that is created with the constructor holds information or data that 
     //is important to the component ;
     //this initializes the state
-    constructor(props) {
 
-        //we should always call these functions
-        //our App component is extending or borrowing functionality from 
-        //the react component base class, this base class has its own constructor function
-        //that goes through some setup to set a react component, we are replaceing or
-        //overwriting that constructor within react component, super is a reference to that parent's constructor
-        super(props); //it is required to do this
+
+    // constructor(props) {
+        //     //we should always call these functions
+        //     //our App component is extending or borrowing functionality from 
+        //     //the react component base class, this base class has its own constructor function
+        //     //that goes through some setup to set a react component, we are replaceing or
+        //     //overwriting that constructor within react component, super is a reference to that parent's constructor
+            //     super(props); //it is required to do this
         
-        //to initialize the state project it is the state object that has information relevant to the component
-        this.state = { lat: null, errorMessage: ''};
-        /**
-         * there is another way to initialize state because the alternate way
-         * is a method that will be seen frequently
-         * to get to it we will get to know a little about component lifecycle method
-         * the lifecycle method is a function that we can optionally define within class based components
-         * if we decide to implement these methods they will be called automatically by react during the component life's cycle
-         * 
-        */
-        //when we initialize state a property insite a state object we must default it to a reasonable
-        //or sensible value, if we dont know what the value will be yet it shoud be set to null
-        //this is the only time we use a direct assignment to the state
-        
+        //     //to initialize the state project it is the state object that has information relevant to the component
+            //     this.state = { lat: null, errorMessage: ''};
+        //     /**
+        //      * there is another way to initialize state because the alternate way
+        //      * is a method that will be seen frequently
+        //      * to get to it we will get to know a little about component lifecycle method
+        //      * the lifecycle method is a function that we can optionally define within class based components
+        //      * if we decide to implement these methods they will be called automatically by react during the component life's cycle
+        //      * 
+        //     */
+        //     //when we initialize state a property insite a state object we must default it to a reasonable
+        //     //or sensible value, if we dont know what the value will be yet it shoud be set to null
+        //     //this is the only time we use a direct assignment to the state
+    // }
+    state = {lat: null, errorMessage: ''};//this is another way to set the state, instead of using the constructor function comented above
+
+    componentDidMount() {
+        //initial data loading request should be placed within componentDidMount
+        //just because it is a best practice
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 //every time we update the state it should be done with the setState function or method
@@ -44,19 +52,14 @@ class App extends React.Component {
                 //do not do that NEVER!! use a direct asigment
                 //the only exceptio is when we initialize the state!!
             },
-            (err) => {
-                
+            (err) => {    
                 this.setState({errorMessage: err.message});
             }
         );
     }
 
 
-
-
-    // react says we have to define render!!!!
-    render() {
-
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
             return(
                 <div>
@@ -66,18 +69,39 @@ class App extends React.Component {
         }
 
         if(!this.state.errorMessage && this.state.lat) {
-            console.log(this.state.lat);
             return(
                 <div>
-                    Latitude: {this.state.lat}
+                    <SeasonDisplay lat={this.state.lat} />{/*state can also be set as props */}
                 </div>
             );
         }
 
         return(
             <div>
-                Loading
+                <Spinner message="Please click allow to continue" />
             </div>
+        );
+    }
+
+
+
+    //the next 2 functions are lifecycle functions placed just to log something to the console
+    //when they run
+    // componentDidMount() {
+    //     console.log('My component was redered to the screen');
+    // }
+    // componentDidUpdate() {
+    //     console.log('My component was just updated-it rerendered!');
+    // }
+
+
+    // react says we have to define render!!!!
+    render() {
+        //the render method is used ONLY to return some jsx 
+        return(
+            <div>
+                {this.renderContent()}
+            </div> {/**before the code for render content was inside the render method but in order to make it easier to maintain it was set to a helper function */}
         );
     }
 }
